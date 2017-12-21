25afe24abb99494bba8036f12d4324f989da66a0
Mikael Jorhult <mikael@jorhult.se>
2017-12-21 21:55:57 +0100

    Fix code style.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Hydrofon\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Hydrofon\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'remember_token' => str_random(10),
     ];
 });
