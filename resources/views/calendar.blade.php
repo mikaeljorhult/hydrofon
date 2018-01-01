d9f225ebbe8c1b509f9bac04ce131ec8b8bf5646
Mikael Jorhult <mikael@jorhult.se>
2018-01-01 19:03:23 +0100

    User can navigate dates in calendar.

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -4,45 +4,12 @@
     @include('partials.object-list')
 
     <main class="main-content">
-        <p>{{ $date }}</p>
+        <header class="calendar-header">
+            <h1>{{ $date->format('Y-m-d') }}</h1>
+            <a href="{{ route('calendar', ['date' => $date->copy()->subDay()->format('Y-m-d')]) }}">Previous</a>
+            <a href="{{ route('calendar', ['date' => $date->copy()->addDay()->format('Y-m-d')]) }}">Next</a>
+        </header>
 
-        <section class="segel">
-            <div class="segel-container">
-                <ul class="segel-grid">
-                    @for($i = 0; $i < 24; $i++)
-                        <li>&nbsp;</li>
-                    @endfor
-                </ul>
-
-                <aside class="segel-ruler">
-                    <ul>
-                        @for($i = 0; $i < 24; $i++)
-                            <li><span {{ now()->hour == $i ? 'class="current"' : '' }}>{{ $i }}</span></li>
-                        @endfor
-                    </ul>
-                </aside>
-
-                <ul class="segel-objects">
-                    @foreach($objects as $object)
-                        <li class="segel-object">
-                            {{ $object->name }}
-
-                            @if($object->bookings->count() > 0)
-                                <ul class="segel-bookings">
-                                    @foreach($object->bookings as $booking)
-                                        <li class="segel-booking"
-                                            style="
-                                                width: {{ $booking->duration / $timestamps['duration'] * 100 }}%;
-                                                left: {{ ($booking->start_time->format('U') - $timestamps['start']) / $timestamps['duration'] * 100 }}%;
-                                            "
-                                        ></li>
-                                    @endforeach
-                                </ul>
-                            @endif
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        </section>
+        @include('partials.segel')
     </main>
 @endsection
---
+++ resources/views/partials/segel.blade.php
@@ -0,0 +1,38 @@
+<section class="segel">
+    <div class="segel-container">
+        <ul class="segel-grid">
+            @for($i = 0; $i < 24; $i++)
+                <li>&nbsp;</li>
+            @endfor
+        </ul>
+
+        <aside class="segel-ruler">
+            <ul>
+                @for($i = 0; $i < 24; $i++)
+                    <li><span {{ now()->hour == $i ? 'class="current"' : '' }}>{{ $i }}</span></li>
+                @endfor
+            </ul>
+        </aside>
+
+        <ul class="segel-objects">
+            @foreach($objects as $object)
+                <li class="segel-object">
+                    {{ $object->name }}
+
+                    @if($object->bookings->count() > 0)
+                        <ul class="segel-bookings">
+                            @foreach($object->bookings as $booking)
+                                <li class="segel-booking"
+                                    style="
+                                            width: {{ $booking->duration / $timestamps['duration'] * 100 }}%;
+                                            left: {{ ($booking->start_time->format('U') - $timestamps['start']) / $timestamps['duration'] * 100 }}%;
+                                            "
+                                ></li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</section>
