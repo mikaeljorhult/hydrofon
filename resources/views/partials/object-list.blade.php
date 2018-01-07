e24f558f820ceaea5ba4c692317a84713ab06717
Mikael Jorhult <mikael@jorhult.se>
2018-01-07 09:33:28 +0100

    Move date input to top of object list.

--- resources/views/partials/object-list.blade.php
+++ resources/views/partials/object-list.blade.php
@@ -1,12 +1,16 @@
 <section class="objectlist">
     {!! Form::open(['route' => 'calendar']) !!}
-        <ul>
-            @each('partials.object-list.category', $categories, 'category')
-            @each('partials.object-list.object', $objects, 'object')
-        </ul>
+        <section class="objectlist-date">
+            {!! Form::text('date', isset($date) ? $date->format('Y-m-d') : now()->format('Y-m-d')) !!}
+            {!! Form::submit('Show calendar', ['class' => 'btn btn-block btn-primary']) !!}
+        </section>
 
-        {!! Form::text('date', isset($date) ? $date->format('Y-m-d') : now()->format('Y-m-d')) !!}
-        {!! Form::submit('Show calendar') !!}
+        <div class="container">
+            <ul>
+                @each('partials.object-list.category', $categories, 'category')
+                @each('partials.object-list.object', $objects, 'object')
+            </ul>
+        </div>
     {!! Form::close() !!}
 
     <a href="#" id="objectlist-toggle" class="objectlist-toggle"></a>
