97f36a6b632d791e64ceef4e28d1a9ab7159bcd0
Mikael Jorhult <mikael@jorhult.se>
2018-01-19 22:01:40 +0100

    Allow Webpack to rewrite URLs.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/admin.scss', 'public/css')
-    .options({
-        processCssUrls: false
-    });
+    .sass('resources/assets/sass/admin.scss', 'public/css');
