a4f0ebf348b6bff3cdffdd528bc891dc34da8173
Mikael Jorhult <mikael@jorhult.se>
2018-01-31 13:33:25 +0100

    Base for flash messages.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,7 @@
 
     <main class="main-content">
         @include('partials/topbar')
-
-        @if(session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
+        @include('flash::message')
 
         <section class="container">
             &nbsp;
