278662de582d3672bc9ef4f6dcadf14792360ac3
Mikael Jorhult <mikael@jorhult.se>
2018-02-01 22:14:04 +0100

    Click to hide a single flash message.

--- resources/assets/js/modules/flashmessages.js
+++ resources/assets/js/modules/flashmessages.js
@@ -3,6 +3,12 @@ let messages = Array.from(document.getElementsByClassName('alert'));
 
 // Check if there are any messages.
 if (messages.length > 0) {
+    // Attach event listener each message.
+    messages.forEach(function (message) {
+        message.addEventListener('click', clickListener, false);
+    });
+
+    // Set timer to hide a message.
     timer = setTimeout(nextMessage, 3000);
 }
 
@@ -26,7 +32,7 @@ function nextMessage() {
  */
 function hideMessage(messageElement) {
     // Remove message once it has been hidden.
-    messageElement.addEventListener('transitionend', function(event) {
+    messageElement.addEventListener('transitionend', function (event) {
         if (event.propertyName === 'visibility') {
             this.remove();
         }
@@ -34,4 +40,16 @@ function hideMessage(messageElement) {
 
     // Add class to trigger hide.
     messageElement.classList.add('alert-hide');
+}
+
+function clickListener(event) {
+    // Stop timer.
+    clearTimeout(timer);
+
+    // Hide current message and start counter to hide next.
+    hideMessage(this);
+    nextMessage();
+
+    // Prevent default click behaviour.
+    event.preventDefault();
 }
