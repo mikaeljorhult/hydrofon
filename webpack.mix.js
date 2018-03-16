bee69dbec1419b73df7e8a64b1c65446a20c14c7
Mikael Jorhult <mikael@jorhult.se>
2018-03-16 21:34:53 +0100

    Version frontend resources.

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,4 +17,5 @@ mix.js('resources/assets/js/app.js', 'public/js')
         'node_modules/segel/dist/index.js'
     ], 'public/js/vendor.js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/admin.scss', 'public/css');
+    .sass('resources/assets/sass/admin.scss', 'public/css')
+    .version();
