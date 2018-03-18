96956a3752d9ebb5a26e7532e1626deeb878a2b4
Mikael Jorhult <mikael@jorhult.se>
2018-03-18 21:30:11 +0100

    Include bookings in Segel.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,15 +6,24 @@ let segelElement = document.getElementById('segel');
 let resources = JSON.parse(segelElement.getAttribute('data-resources'));
 
 for (let resource of resources) {
-    console.log({
-        id: resource.id,
-        name: resource.name
-    });
-
     Segel.resources.add({
         id: resource.id,
         name: resource.name
     });
+
+    if (resource.bookings.length > 0) {
+        for (let booking of resource.bookings) {
+            Segel.bookings.add({
+                id: booking.id,
+                resource: resource.id,
+                start: booking.start_time,
+                end: booking.end_time
+            });
+        }
+    }
 }
+
+Segel.instance.$store.state.start = parseInt(segelElement.getAttribute('data-start'));
+Segel.instance.$store.state.end = parseInt(segelElement.getAttribute('data-end'));
 
 Segel('#segel');
