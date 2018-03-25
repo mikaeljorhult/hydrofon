b51b64da6a36adb61de4d5ecfbd829440fc11127
Mikael Jorhult <mikael@jorhult.se>
2018-03-25 21:36:21 +0200

    Move calendar JavaScript to own module.

---
+++ resources/assets/js/modules/segel.js
@@ -0,0 +1,24 @@
+let segelElement = document.getElementById('segel');
+let resources = JSON.parse(segelElement.getAttribute('data-resources'));
+
+for (let resource of resources) {
+    Segel.resources.add({
+        id: resource.id,
+        name: resource.name
+    });
+
+    if (resource.bookings.length > 0) {
+        for (let booking of resource.bookings) {
+            Segel.bookings.add({
+                id: booking.id,
+                resource: resource.id,
+                start: booking.start_time,
+                end: booking.end_time
+            });
+        }
+    }
+}
+
+Segel.time.set(segelElement.getAttribute('data-start'), segelElement.getAttribute('data-end'));
+
+// Segel('#segel');
