de115e4b9168482049e4a666013229de8e8fcdc4
Mikael Jorhult <mikael@jorhult.se>
2018-04-11 21:39:28 +0200

    Test filtering of bookings index.

---
+++ tests/Feature/Bookings/IndexTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature\Bookings;
+
+use Hydrofon\Booking;
+use Hydrofon\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Bookings are listed in index.
+     *
+     * @return void
+     */
+    public function testBookingsAreListed()
+    {
+        $booking = factory(Booking::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('bookings')
+             ->assertSuccessful()
+             ->assertSee($booking->resource->name);
+    }
+
+    /**
+     * Bookings index can be filtered by resource name.
+     *
+     * @return void
+     */
+    public function testBookingsCanBeFilteredByResourceName()
+    {
+        $visibleBooking    = factory(Booking::class)->create();
+        $notVisibleBooking = factory(Booking::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('bookings?'.http_build_query([
+                     'filter' => $visibleBooking->resource->name,
+                 ]))
+             ->assertSuccessful()
+             ->assertSee($visibleBooking->resource->name)
+             ->assertDontSee($notVisibleBooking->resource->name);
+    }
+
+    /**
+     * Bookings index can be filtered by user name.
+     *
+     * @return void
+     */
+    public function testBookingsCanBeFilteredByUserName()
+    {
+        $visibleBooking    = factory(Booking::class)->create();
+        $notVisibleBooking = factory(Booking::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('bookings?'.http_build_query([
+                     'filter' => $visibleBooking->user->name,
+                 ]))
+             ->assertSuccessful()
+             ->assertSee($visibleBooking->user->name)
+             ->assertDontSee($notVisibleBooking->user->name);
+    }
+}
