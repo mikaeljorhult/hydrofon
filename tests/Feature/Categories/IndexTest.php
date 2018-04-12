effeccf3c51e95c1f36cc700e1986804ed332ab2
Mikael Jorhult <mikael@jorhult.se>
2018-04-12 18:37:32 +0200

    Test filtering of indexes.

---
+++ tests/Feature/Categories/IndexTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Categories;
+
+use Hydrofon\Category;
+use Hydrofon\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Categories are listed in index.
+     *
+     * @return void
+     */
+    public function testCategoriesAreListed()
+    {
+        $category = factory(Category::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('categories')
+             ->assertSuccessful()
+             ->assertSee($category->name);
+    }
+
+    /**
+     * Categories index can be filtered by name.
+     *
+     * @return void
+     */
+    public function testCategoriesCanBeFilteredByName()
+    {
+        $visibleCategory    = factory(Category::class)->create();
+        $notVisibleCategory = factory(Category::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('categories?'.http_build_query([
+                     'filter' => $visibleCategory->name,
+                 ]))
+             ->assertSuccessful()
+             ->assertSee($visibleCategory->name)
+             ->assertDontSee($notVisibleCategory->name);
+    }
+}
---
+++ tests/Feature/Users/IndexTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Users;
+
+use Hydrofon\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Users are listed in index.
+     *
+     * @return void
+     */
+    public function testUsersAreListed()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('users')
+             ->assertSuccessful()
+             ->assertSee($user->name);
+    }
+
+    /**
+     * Users index can be filtered by e-mail address.
+     *
+     * @return void
+     */
+    public function testUsersCanBeFilteredByEmail()
+    {
+        $visibleUser    = factory(User::class)->create();
+        $notVisibleUser = factory(User::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('users?'.http_build_query([
+                     'filter' => $visibleUser->email,
+                 ]))
+             ->assertSuccessful()
+             ->assertSee($visibleUser->email)
+             ->assertDontSee($notVisibleUser->email);
+    }
+
+    /**
+     * Users index can be filtered by name.
+     *
+     * @return void
+     */
+    public function testUsersCanBeFilteredByName()
+    {
+        $visibleUser    = factory(User::class)->create();
+        $notVisibleUser = factory(User::class)->create();
+
+        $this->actingAs(factory(User::class)->states('admin')->create())
+             ->get('users?'.http_build_query([
+                     'filter' => $visibleUser->name,
+                 ]))
+             ->assertSuccessful()
+             ->assertSee($visibleUser->name)
+             ->assertDontSee($notVisibleUser->name);
+    }
+}
