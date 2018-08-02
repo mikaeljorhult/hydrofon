3622daaa944032dcd88a924ec4ceaec25416f581
Mikael Jorhult <mikael@jorhult.se>
2018-08-02 13:09:22 +0200

    Restructure top navigation bar.

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -2,22 +2,26 @@
     <nav class="topbar">
         <section class="topbar-desk">
             {!! Form::open(['route' => 'desk']) !!}
-                <label for="search">
-                    @svg('magnify')
-                    {!! Form::search('search', $search ?? null, ['placeholder' => 'Search user...']) !!}
+                @svg('magnify')
+
+                <label for="search" class="screen-reader">
+                    Search for user
                 </label>
 
+                {!! Form::text('search', $search ?? null, ['placeholder' => 'Search user...']) !!}
                 {!! Form::submit('Search', ['class' => 'btn btn-primary screen-reader']) !!}
             {!! Form::close() !!}
         </section>
 
         <section class="topbar-impersonation">
             {!! Form::open(['route' => 'impersonation']) !!}
-                <label for="user_id">
-                    @svg('account-switch')
-                    {!! Form::select('user_id', \Hydrofon\User::pluck('name', 'id'), session()->get('impersonate', null), ['placeholder' => 'Impersonate user...']) !!}
+                @svg('account-switch')
+
+                <label for="user_id" class="screen-reader">
+                    Select user to impersonate
                 </label>
 
+                {!! Form::select('user_id', \Hydrofon\User::pluck('name', 'id'), session()->get('impersonate', null), ['placeholder' => 'Impersonate user...']) !!}
                 {!! Form::submit('Impersonate', ['class' => 'btn btn-primary screen-reader']) !!}
             {!! Form::close() !!}
         </section>
