a6c7f401943fb45850b10791a1ab8c888c3dd90b
Mikael Jorhult <mikael@jorhult.se>
2018-11-25 14:12:43 +0100

    Fix issue when Segel is not found on page.

--- resources/js/modules/segel.js
+++ resources/js/modules/segel.js
@@ -1,24 +1,27 @@
 let segelElement = document.getElementById('segel');
-let resources = JSON.parse(segelElement.getAttribute('data-resources'));
 
-for (let resource of resources) {
-    Segel.resources.add({
-        id: resource.id,
-        name: resource.name
-    });
+if (segelElement !== null) {
+    let resources = JSON.parse(segelElement.getAttribute('data-resources'));
 
-    if (resource.bookings.length > 0) {
-        for (let booking of resource.bookings) {
-            Segel.bookings.add({
-                id: booking.id,
-                resource: resource.id,
-                start: booking.start_time,
-                end: booking.end_time
-            });
+    for (let resource of resources) {
+        Segel.resources.add({
+            id: resource.id,
+            name: resource.name
+        });
+
+        if (resource.bookings.length > 0) {
+            for (let booking of resource.bookings) {
+                Segel.bookings.add({
+                    id: booking.id,
+                    resource: resource.id,
+                    start: booking.start_time,
+                    end: booking.end_time
+                });
+            }
         }
     }
+
+    Segel.time.set(segelElement.getAttribute('data-start'), segelElement.getAttribute('data-end'));
+
+    // Segel('#segel');
 }
-
-Segel.time.set(segelElement.getAttribute('data-start'), segelElement.getAttribute('data-end'));
-
-// Segel('#segel');
