f6ffb0a232b9dbc77b2f3df5f9c97ba07dc669b8
Mikael Jorhult <mikael@jorhult.se>
2019-05-28 20:02:16 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Resources/Resource.php
+++ app/Http/Resources/Resource.php
@@ -9,7 +9,7 @@ class Resource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      *
      * @return array
      */
