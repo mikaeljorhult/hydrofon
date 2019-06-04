86658ffc355e719ceeb3feeba2657cb56f035954
Mikael Jorhult <mikael@jorhult.se>
2019-06-04 19:08:17 +0200

    Fix issue with variables for JavaScript resource tree.

--- app/Http/ViewComposers/ResourceListComposer.php
+++ app/Http/ViewComposers/ResourceListComposer.php
@@ -13,37 +13,18 @@ class ResourceListComposer
     /**
      * Bind data to the view.
      *
-     * @param View $view
+     * @param  View  $view
      *
      * @return void
      */
     public function compose(View $view)
     {
-        $categories = Category::with(['groups'])
-                              ->orderBy('name')
-                              ->get();
+        $categories    = $this->getCategories();
+        $rootResources = $this->getRootResources();
 
-        $rootResources = Resource::with(['groups'])
-                                 ->orderBy('name')
-                                 ->doesntHave('categories')
-                                 ->get(['id', 'name', 'is_facility']);
-
-        $jsResources = Resource::with(['categories', 'groups'])
-                               ->whereHas('categories', function ($query) use ($categories) {
-                                   $query->whereIn('id', $categories->pluck('id')->toArray());
-                               })
-                               ->orDoesntHave('categories')
-                               ->orderBy('name')
-                               ->get(['id', 'name', 'is_facility'])
-                               ->values()
-                               ->map(function ($resource) {
-                                   return new ResourceResource($resource);
-                               });
-
-        $jsCategories = $categories
-            ->map(function ($category) {
-                return new CategoryResource($category);
-            });
+        $flatCategories = $categories->toFlatTree();
+        $jsCategories   = $this->getJavaScriptCategories($flatCategories);
+        $jsResources    = $this->getJavaScriptResources($flatCategories);
 
         $view->with([
             'categories'   => $categories->load('resources', 'resources.groups')->toTree(),
@@ -51,5 +32,68 @@ class ResourceListComposer
             'jsCategories' => $jsCategories->toArray(),
             'jsResources'  => $jsResources->toArray(),
         ]);
+    }
+
+    /**
+     * Retrieve all categories within same group as current user.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    private function getCategories()
+    {
+        return Category::with(['groups'])
+                       ->orderBy('name')
+                       ->get();
+    }
+
+    /**
+     * Retrieve resources within same group as current user that dosen't belong to any categories.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    private function getRootResources()
+    {
+        return Resource::with(['groups'])
+                       ->orderBy('name')
+                       ->whereDoesntHave('categories', function ($query) {
+                           $query->withoutGlobalScopes();
+                       })
+                       ->get(['id', 'name', 'is_facility']);
+    }
+
+    /**
+     * Map all categories to JavaScript resources.
+     *
+     * @param $categories
+     * @return \Illuminate\Support\Collection
+     */
+    private function getJavaScriptCategories($categories)
+    {
+        return $categories->map(function ($category) {
+            return new CategoryResource($category);
+        });
+    }
+
+    /**
+     * Retrieve resources and map them to JavaScript resources.
+     *
+     * @param $categories
+     * @return \Illuminate\Support\Collection
+     */
+    private function getJavaScriptResources($categories)
+    {
+        return Resource::with(['categories', 'groups'])
+                       ->whereHas('categories', function ($query) use ($categories) {
+                           $query->whereIn('id', $categories->pluck('id')->toArray());
+                       })
+                       ->orWhereDoesntHave('categories', function ($query) {
+                           $query->withoutGlobalScopes();
+                       })
+                       ->orderBy('name')
+                       ->get(['id', 'name', 'is_facility'])
+                       ->values()
+                       ->map(function ($resource) {
+                           return new ResourceResource($resource);
+                       });
     }
 }
