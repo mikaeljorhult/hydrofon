5b73b69c7b91a09839dd6490b197e8bc86ef2b53
Mikael Jorhult <mikael@jorhult.se>
2019-12-26 19:57:29 +0100

    Fix selector when focusing first field in tables.

--- resources/views/livewire/partials/table-scripts.blade.php
+++ resources/views/livewire/partials/table-scripts.blade.php
@@ -2,7 +2,7 @@
     <script type="text/javascript">
         document.addEventListener('DOMContentLoaded', function () {
             @this.on('editing', function () {
-                @this.root.el.querySelector('td:nth-of-type(2)').querySelector('select, input').focus();
+                @this.root.el.querySelector('.is-editing').querySelector('select, input').focus();
             });
         });
     </script>
