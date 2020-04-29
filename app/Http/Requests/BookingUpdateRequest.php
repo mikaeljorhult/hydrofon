7c81d30bb12d0ef2d0524b23c4fb02f2397ee715
Mikael Jorhult <mikael@jorhult.se>
2020-04-29 20:30:35 +0200

    Bookings can be dragged and resized.

--- app/Http/Requests/BookingUpdateRequest.php
+++ app/Http/Requests/BookingUpdateRequest.php
@@ -24,17 +24,6 @@ class BookingUpdateRequest extends FormRequest
     }
 
     /**
-     * Prepare the data for validation.
-     *
-     * @return void
-     */
-    protected function prepareForValidation()
-    {
-        $this->conformDateFormat();
-        $this->prepareApiFields();
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -51,49 +40,5 @@ class BookingUpdateRequest extends FormRequest
             'start_time'  => ['required', 'date', 'required_with:resource_id', 'before:end_time'],
             'end_time'    => ['required', 'date', 'required_with:resource_id', 'after:start_time'],
         ];
-    }
-
-    /**
-     * Rename API fields and parse timestamps as UNIX timestamps.
-     */
-    protected function prepareApiFields()
-    {
-        $fields = collect(['resource_id' => 'resource', 'start_time' => 'start', 'end_time' => 'end'])
-            ->filter(function ($apiField, $dbField) {
-                return ! $this->has($dbField) && $this->has($apiField);
-            })
-            ->mapWithKeys(function ($apiField, $dbField) {
-                $value = $this->get($apiField);
-
-                if (Str::contains($dbField, '_time')) {
-                    $value = Carbon::parse('@'.$value);
-                }
-
-                return [$dbField => $value];
-            });
-
-        $this->merge($fields->toArray());
-    }
-
-    /**
-     * Allow dates to be entered both with and without seconds.
-     */
-    protected function conformDateFormat()
-    {
-        $fields = collect(['start_time', 'end_time'])
-            ->filter(function ($field) {
-                return $this->has($field);
-            })
-            ->mapWithKeys(function ($field) {
-                $value = $this->get($field);
-
-                if ($value && preg_match('/\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}$/', $value)) {
-                    return [$field => Carbon::parse($value.':00')];
-                }
-
-                return [$field => $value];
-            });
-
-        $this->merge($fields->toArray());
     }
 }
