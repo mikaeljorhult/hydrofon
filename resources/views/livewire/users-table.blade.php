17d57a2c4f681d9c202a6ed505903262d8487fe6
Mikael Jorhult <mikael@jorhult.se>
2020-05-08 21:52:53 +0200

    Responsive select box for related groups.

--- resources/views/livewire/users-table.blade.php
+++ resources/views/livewire/users-table.blade.php
@@ -74,7 +74,7 @@
                         <td>&nbsp;</td>
                         <td colspan="3">
                             <div class="flex pb-4">
-                                <div class="w-1/3">
+                                <div class="w-full lg:w-1/2 xl:w-1/3">
                                     <label class="block mb-2 text-xs uppercase">Groups</label>
                                     <select
                                         name="groups[]"
