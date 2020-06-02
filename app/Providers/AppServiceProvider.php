2e8127d79d3972c5b0377bde6ae77e299a6064e1
Mikael Jorhult <mikael@jorhult.se>
2020-06-02 21:16:49 +0200

    Remove old unused code.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,30 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Allow foreign keys with SQLite.
-        if (DB::connection() instanceof SQLiteConnection) {
-            DB::statement(DB::raw('PRAGMA foreign_keys=1'));
-        }
-
         // Custom Blade if statement for administrators.
         Blade::if('admin', function () {
             return auth()->check() && auth()->user()->isAdmin();
-        });
-
-        // Collection macro to walk through and return nested resources.
-        Collection::macro('nested', function ($type, $within = null, $includeSelf = true) {
-            $within = $within ?? $type;
-
-            $traversables = collect($this->items);
-            $items = collect($includeSelf ? $this->items : []);
-
-            foreach ($traversables as $traversable) {
-                $items = $items
-                    ->merge($traversable->$type)
-                    ->merge($traversable->$within->nested($type, $within, false));
-            }
-
-            return $items;
         });
     }
 
