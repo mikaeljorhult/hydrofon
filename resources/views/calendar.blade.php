85312e574fcf29410cc097c1b60d5bb042cecf39
Mikael Jorhult <mikael@jorhult.se>
2020-06-11 23:22:49 +0200

    Remove unnecessary CSS.

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -8,34 +8,41 @@
 
 @section('content')
     <section class="container">
-        <div
-            class="calendar-header"
-            x-data="{
-                date: '{{ $date->format('Y-m-d') }}'
-            }"
-            x-init="
-                window.livewire.on('dateChanged', (segelDate) => {
-                    date = segelDate.date;
-                })
-            "
-        >
-            <h1 x-text="date">{{ $date->format('Y-m-d') }}</h1>
-            <a
-                title="Previous"
-                href="{{ route('calendar', [$date->copy()->subDay()->format('Y-m-d')]) }}"
-                x-on:click.prevent="HYDROFON.Segel.component.call('previousTimeScope');"
+        <div class="flex justify-between items-baseline mb-4">
+            <div
+                class="calendar-header flex items-baseline"
+                x-data="{
+                    date: '{{ $date->format('Y-m-d') }}'
+                }"
+                x-init="
+                    window.livewire.on('dateChanged', (segelDate) => {
+                        date = segelDate.date;
+                    })
+                "
             >
-                @svg('cheveron-left', 'w-6')
-                <span class="screen-reader">Previous</span>
-            </a>
-            <a
-                title="Next"
-                href="{{ route('calendar', [$date->copy()->addDay()->format('Y-m-d')]) }}"
-                x-on:click.prevent="HYDROFON.Segel.component.call('nextTimeScope');"
-            >
-                <span class="screen-reader">Next</span>
-                @svg('cheveron-right', 'w-6')
-            </a>
+                <h1
+                    class="order-2 my-0 mx-1 text-3xl"
+                    x-text="date"
+                >{{ $date->format('Y-m-d') }}</h1>
+                <a
+                    class="order-1"
+                    title="Previous"
+                    href="{{ route('calendar', [$date->copy()->subDay()->format('Y-m-d')]) }}"
+                    x-on:click.prevent="HYDROFON.Segel.component.call('previousTimeScope');"
+                >
+                    @svg('cheveron-left', 'w-5')
+                    <span class="screen-reader">Previous</span>
+                </a>
+                <a
+                    class="order-3"
+                    title="Next"
+                    href="{{ route('calendar', [$date->copy()->addDay()->format('Y-m-d')]) }}"
+                    x-on:click.prevent="HYDROFON.Segel.component.call('nextTimeScope');"
+                >
+                    <span class="screen-reader">Next</span>
+                    @svg('cheveron-right', 'w-5')
+                </a>
+            </div>
         </div>
 
         @livewire('segel', ['resources' => $resources, 'timestamps' => $timestamps])
