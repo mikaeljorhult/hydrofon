568ab7f4a35647cae6e70c3310aacc9761424808
Mikael Jorhult <mikael@jorhult.se>
2021-10-28 23:01:13 +0200

    More noticable notification indicator.

--- resources/views/livewire/notifications-indicator.blade.php
+++ resources/views/livewire/notifications-indicator.blade.php
@@ -1,8 +1,12 @@
 <a
     href="{{ route('notifications') }}"
-    class="bg-white p-1 rounded-full {{ $hasUnreadNotifications ? 'text-red-600 hover:text-red-700' : 'text-gray-400 hover:text-gray-500' }} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-300"
+    class="group relative bg-white p-1 rounded-full {{ $hasUnreadNotifications ? 'text-red-600 hover:text-red-700' : 'text-gray-400 hover:text-gray-500' }} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-300"
     wire:poll.keep-alive.30s
 >
     <span class="sr-only">View notifications</span>
     <x-heroicon-o-bell class="w-6 h-6" />
+
+    @if($hasUnreadNotifications)
+        <span class="block w-3 h-3 absolute top-1 right-1 bg-red-600 group-hover:bg-red-700 border-2 border-white rounded-full"></span>
+    @endif
 </a>
