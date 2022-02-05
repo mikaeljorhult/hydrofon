5e27ef179a54e87c3f37f12259be6c572ed0f460
Mikael Jorhult <mikael@jorhult.se>
2022-02-05 20:15:11 +0100

    Redirect to calendar as home route is not yet used.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         return auth()->check()
-            ? view('home')
+            ? redirect('calendar')
             : view('welcome');
     }
 }
