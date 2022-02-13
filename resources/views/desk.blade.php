0d3883b53d89498dac36812ef357f76794fa53f2
Mikael Jorhult <mikael@jorhult.se>
2022-02-13 16:28:47 +0100

    Add search form on desk view.

--- resources/views/desk.blade.php
+++ resources/views/desk.blade.php
@@ -13,6 +13,31 @@
             {{ $bookings->appends(['filter' => request()->get('filter'), 'sort' => request()->get('sort')])->links() }}
         @elseif($search)
             <p>No resource or user was found.</p>
+        @else
+            <section class="mx-auto md:max-w-2xl">
+                <x-heading title="Desk" :url="route('desk')" />
+
+                <form action="{{ route('desk') }}" method="post">
+                    @csrf
+
+                    <x-forms.label for="desk-search" class="sr-only">
+                        Search for resource or user
+                    </x-forms.label>
+
+                    <div class="flex gap-x-2">
+                        <x-forms.input
+                            type="search"
+                            id="desk-search"
+                            name="search"
+                            placeholder="Search for resource or user"
+                        />
+
+                        <x-forms.button>
+                            Search
+                        </x-forms.button>
+                    </div>
+                </form>
+            </section>
         @endif
     </section>
 @endsection
