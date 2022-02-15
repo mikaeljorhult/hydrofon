655c7c56134c10419231595f7d629f1ea9fb6edc
Shift <shift@laravelshift.com>
2022-02-15 19:15:28 +0000

    Adopt anonymous migrations

--- database/migrations/2017_12_25_154702_create_checkouts_table.php
+++ database/migrations/2017_12_25_154702_create_checkouts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCheckoutsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -38,4 +38,4 @@ class CreateCheckoutsTable extends Migration
     {
         Schema::dropIfExists('checkouts');
     }
-}
+};
--- database/migrations/2019_02_19_091520_create_category_group_table.php
+++ database/migrations/2019_02_19_091520_create_category_group_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoryGroupTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -36,4 +36,4 @@ class CreateCategoryGroupTable extends Migration
     {
         Schema::dropIfExists('category_group');
     }
-}
+};
