80bbd4d584a886410b52f6bdef8156186f86de23
Mikael Jorhult <mikael@jorhult.se>
2022-03-23 21:26:50 +0100

    Administrator middleware should handle visitors.

--- app/Http/Middleware/Administrator.php
+++ app/Http/Middleware/Administrator.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Auth\AuthenticationException;
 
 class Administrator
 {
@@ -12,9 +13,16 @@ class Administrator
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
+     * @throws \Illuminate\Auth\AuthenticationException
      */
     public function handle($request, Closure $next)
     {
+        // User is not logged in.
+        if ($request->user() === null) {
+            throw new AuthenticationException('Unauthenticated.', [], route('login'));
+        }
+
+        // User is not an administrator.
         if (! $request->user()->isAdmin()) {
             abort(403);
         }
