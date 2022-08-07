b72315f045bb0c169bcb51459bd2f6c40d69ca63
Mikael Jorhult <mikael@jorhult.se>
2022-08-07 22:14:48 +0200

    Make Vite play nice with Valet.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import fs from 'fs';
+import { resolve } from 'path';
+import { homedir } from 'os';
+
+const host = 'hydrofon.test';
+
 export default defineConfig({
     plugins: [
         laravel([
@@ -8,4 +14,26 @@ export default defineConfig({
             'resources/js/app.js',
         ]),
     ],
+
+    server: serverConfig(host)
 });
+
+function serverConfig (host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath) || !fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        host: host,
+        hmr: {
+            host: host
+        },
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        }
+    }
+}
