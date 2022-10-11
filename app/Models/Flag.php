9b1070b85b929e3eb9b792e044a4a24b27eee3bb
Mikael Jorhult <mikael@jorhult.se>
2022-10-11 18:23:03 +0200

    Safe list flag colors.

--- app/Models/Flag.php
+++ app/Models/Flag.php
@@ -9,6 +9,12 @@ class Flag extends Model
 {
     use Sushi;
 
+    /**
+     * Flags available for resources.
+     * Colors defined in array need to be safelisted in Tailwind CSS configuration.
+     *
+     * @var array
+     */
     protected array $rows = [
         ['abbr' => 'broken', 'name' => 'Broken', 'color' => 'text-red-600'],
         ['abbr' => 'dirty', 'name' => 'Dirty', 'color' => 'text-yellow-600'],
