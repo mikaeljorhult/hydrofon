3751885d30ccd20b6a168d7f6831da40858b3899
Mikael Jorhult <mikael@jorhult.se>
2022-10-21 15:06:44 +0200

    Match styles between identifiers table and other tables.

--- resources/views/identifiers/index.blade.php
+++ resources/views/identifiers/index.blade.php
@@ -34,20 +34,31 @@
 
             <tbody>
                 @forelse($identifiable->identifiers as $identifier)
-                    <tr>
+                    <tr class="{{ $loop->odd ? 'odd' : 'even bg-slate-50' }} group hover:bg-red-50">
                         <td data-title="&nbsp;"></td>
                         <td data-title="Name">
                             <a href="{{ route($identifiable->getTable().'.identifiers.edit', [$identifiable, $identifier]) }}">{{ $identifier->value }}</a>
                         </td>
-                        <td data-title="&nbsp;" class="table-actions">
-                            <form action="{{ route($identifiable->getTable().'.identifiers.destroy', [$identifiable, $identifier]) }}" method="post">
-                                @method('delete')
-                                @csrf
+                        <td data-title="&nbsp;" class="flex justify-end">
+                            <a
+                                class="invisible group-hover:visible ml-2 p-1 border border-solid border-gray-300 text-gray-500 rounded hover:text-red-700 hover:border-red-700"
+                                href="{{ route('resources.identifiers.edit', [$identifiable, $identifier]) }}"
+                                title="Edit"
+                            ><x-heroicon-m-pencil class="w-4 h-4 fill-current" /></a>
 
-                                <button type="submit" title="Delete">
-                                    Delete
-                                </button>
-                            </form>
+                            <button
+                                class="invisible group-hover:visible ml-2 p-1 border border-solid border-gray-300 text-gray-500 rounded hover:text-red-700 hover:border-red-700"
+                                form="deleteform-{{ $identifier->id }}"
+                                type="submit"
+                                title="Delete"
+                            ><x-heroicon-m-x-mark class="w-4 h-4 fill-current" /></button>
+
+                            <div class="hidden">
+                                <form action="{{ route($identifiable->getTable().'.identifiers.destroy', [$identifiable, $identifier]) }}" method="post" id="deleteform-{{ $identifier->id }}">
+                                    @method('delete')
+                                    @csrf
+                                </form>
+                            </div>
                         </td>
                     </tr>
                 @empty
