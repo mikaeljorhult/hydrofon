8ddb2756d7602a44abf7063fcf0381ecaabb5661
Shift <shift@laravelshift.com>
2024-03-15 14:32:13 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Console\Commands\OverdueBookingNotification;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('model:prune')->daily();
+Schedule::command(OverdueBookingNotification::class)->everyFifteenMinutes();
